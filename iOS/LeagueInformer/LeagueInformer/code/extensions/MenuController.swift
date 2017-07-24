8b512a2ececd65f5df21378ec102dc6dc52fed07
dlernatovich <dlernatovich@geeksforless.net>
2017-07-24 20:01:50 +0300

    Minor changes

--- iOS/LeagueInformer/LeagueInformer/code/extensions/MenuController.swift
+++ iOS/LeagueInformer/LeagueInformer/code/extensions/MenuController.swift
@@ -15,6 +15,18 @@ class MenuController: UIViewController, UITableViewDelegate, UITableViewDataSour
     /// {@link String} value of the cell identifier
     private static let K_CELL_IDENTIFIER: String! = "cellMenu";
 
+    /// Enum which provide the containing of the images of the menu items
+    fileprivate enum Images: String {
+        case home = "img_menu_home"
+        case news = "img_menu_news"
+    }
+    
+    /// Enum which provide the keeping title for the menu items
+    fileprivate enum Titles: String {
+        case home = "Home"
+        case news = "News"
+    }
+
     //MARK: - Variables
     
     /// Instance of the {@link UITableView}
@@ -51,7 +63,7 @@ class MenuController: UIViewController, UITableViewDelegate, UITableViewDataSour
     ///   - section: {@link Int} value of the section number
     /// - Returns: numbers of rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 9;
+        return 2;
     }
     
     /// Method which provide the define of the cell for index
@@ -62,6 +74,21 @@ class MenuController: UIViewController, UITableViewDelegate, UITableViewDataSour
     /// - Returns: cell for index
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: MenuController.K_CELL_IDENTIFIER, for: indexPath);
+        if let image = cell.viewWithTag(1) as? UIImageView {
+            if(indexPath.row == 0){
+                image.image = UIImage(named: Images.home.rawValue);
+            }else if(indexPath.row == 1){
+                image.image = UIImage(named: Images.news.rawValue);
+            }
+        }
+        
+        if let label = cell.viewWithTag(2) as? UILabel {
+            if(indexPath.row == 0){
+                label.text = Titles.home.rawValue;
+            }else if(indexPath.row == 1){
+                label.text = Titles.news.rawValue;            }
+        }
+
         return cell;
     }
     
