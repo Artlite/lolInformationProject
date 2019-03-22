064a533a9a4c7b3daacb0db8557efe731203d460
dlernatovich <dlernatovich@geeskforless.net>
2019-03-22 16:20:11 +0200

    Updated the library

--- LOLInformer/app/src/main/java/com/artlite/collapsinglayouttest/core/application/CurrentApplication.java
+++ LOLInformer/app/src/main/java/com/artlite/collapsinglayouttest/core/application/CurrentApplication.java
@@ -10,7 +10,13 @@ import com.artlite.bslibrary.constants.BSTypeface;
 import com.artlite.bslibrary.core.BSInstance;
 import com.artlite.bslibrary.helpers.log.BSLogHelper;
 import com.artlite.bslibrary.managers.BSContextManager;
+import com.artlite.bslibrary.managers.BSCryptSharedPreferenceManager;
 import com.artlite.collapsinglayouttest.utils.AppLogger;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by dlernatovich on 7/23/15.
@@ -30,6 +36,27 @@ public final class CurrentApplication extends Application {
     public void onCreate() {
         super.onCreate();
         BSInstance.init(this, BSTypeface.EXO_2);
+
+        final String text = "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit Sed Do Eiusmod Tempor Inc";
+        final Date date = new Date();
+        final List<String> strings = new ArrayList<>(Arrays.asList(new String[]{
+                "Lorem Ipsum Dolor Sit Amet Co",
+                "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit Sed Do Eiusmo",
+                "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit Sed Do Eiusmod Temp"
+
+        }));
+
+        BSCryptSharedPreferenceManager.save(date, "K_DATE");
+        BSCryptSharedPreferenceManager.save(text, "K_TEXT");
+        BSCryptSharedPreferenceManager.save(strings, "K_TEXTS");
+
+        final String text1 = BSCryptSharedPreferenceManager.getString("K_TEXT");
+        final Date date1 = BSCryptSharedPreferenceManager.getDate("K_DATE");
+        final List<String> strings1 = BSCryptSharedPreferenceManager.getList("K_TEXTS");
+
+        BSLogHelper.log(this, "onCreate1", null, text1);
+        BSLogHelper.log(this, "onCreate1", null, date1);
+        BSLogHelper.log(this, "onCreate1", null, strings1);
     }
 
     /**
